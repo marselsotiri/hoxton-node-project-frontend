17ee37b35c35d2a6e516d23db01555201897ae45
Geri <luga.geri@gmail.com>
2022-03-21 13:01:36 +0100

    Moved css files and created public folder

---
+++ src/Pages/LogIn/LogIn.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../../assets/Login.css'
+
+const LogIn = () => {
+    return (
+        <section className='log_in'>
+            <h2>Log in via Mobile Number</h2>
+            <form className='log_in'>
+                <label htmlFor='region'>
+                    Region <input type='text' name='region' />
+                </label>
+                <label htmlFor='phone'>
+                    Phone <input type='number' name='phone' />
+                </label>
+                <span> Log in with email</span>
+                <button type='submit'>Continue</button>
+            </form>
+        </section>
+    );
+};
+
+export default LogIn;
--- src/Pages/Start/Start.tsx
+++ src/Pages/Start/Start.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Start.css';
+import '../../assets/Start.css';
 
 const Start = () => {
     const navigate = useNavigate();
