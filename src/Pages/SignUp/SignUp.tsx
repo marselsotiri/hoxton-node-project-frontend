922732bbf17712a5f9a959a1747462acc78de50b
Geri <luga.geri@gmail.com>
2022-03-22 15:51:14 +0100

    Added validation to sign up page

--- src/Pages/SignUp/SignUp.tsx
+++ src/Pages/SignUp/SignUp.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import SignUpForm from '../../Components/SignUpForm';
 //@ts-ignore
 import { useStore } from '../../Store/store';
+import { validate } from '../../utils/api';
 
 const SignUp = () => {
+    const setCurrentUser = useStore((store: any) => store.setCurrentUser);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        validate().then((data) => {
+            if (data.error) return;
+            setCurrentUser(data.user);
+            navigate('/home');
+        });
+    }, []);
+
     return (
         <section className='log_in'>
             <h2>Create an account</h2>
-            
+
             <SignUpForm />
         </section>
     );
